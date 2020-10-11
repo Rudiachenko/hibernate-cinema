527d6a0261155e487e1ea0cfc74afb7fcd87aa3a
Fedia <fedia.rudiachenko@gmail.com>
2020-10-11 18:26:09 +0300

    added annotation

--- src/main/java/com/dev/cinema/model/Ticket.java
+++ src/main/java/com/dev/cinema/model/Ticket.java
@@ -1,12 +1,9 @@
 package com.dev.cinema.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "ticket")
 public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
