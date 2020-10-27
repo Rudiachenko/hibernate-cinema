ad7ec8c1dd6901b17e3c2bc5e06126649b811737
Fedia <fedia.rudiachenko@gmail.com>
2020-10-27 13:59:08 +0200

    added CinemaHallController

---
+++ src/main/java/com/dev/cinema/service/impl/CinemaHallMapper.java
@@ -0,0 +1,24 @@
+package com.dev.cinema.service.impl;
+
+import com.dev.cinema.model.CinemaHall;
+import com.dev.cinema.model.dto.CinemaHallRequestDto;
+import com.dev.cinema.model.dto.CinemaHallResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CinemaHallMapper {
+    public CinemaHall toCinemaHallModel(CinemaHallRequestDto cinemaHallRequestDto) {
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setDescription(cinemaHallRequestDto.getDescription());
+        cinemaHall.setCapacity(cinemaHallRequestDto.getCapacity());
+        return cinemaHall;
+    }
+
+    public CinemaHallResponseDto toCinemaHallResponseDto(CinemaHall cinemaHall) {
+        CinemaHallResponseDto cinemaHallResponseDto = new CinemaHallResponseDto();
+        cinemaHallResponseDto.setId(cinemaHall.getId());
+        cinemaHallResponseDto.setCapacity(cinemaHall.getCapacity());
+        cinemaHallResponseDto.setDescription(cinemaHall.getDescription());
+        return cinemaHallResponseDto;
+    }
+}
