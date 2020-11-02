3f412248d2ed7350abee53fdbb0cf02759a5a823
Fedia <fedia.rudiachenko@gmail.com>
2020-11-02 20:01:41 +0200

    added inject data

--- src/main/java/com/dev/cinema/config/SecurityConfig.java
+++ src/main/java/com/dev/cinema/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.dev.cinema.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,23 +13,32 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final UserDetailsService userDetailsService;
-
-    @Autowired
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
+//    private final UserDetailsService userDetailsService;
+//
+//    @Autowired
+//    public SecurityConfig(UserDetailsService userDetailsService) {
+//        this.userDetailsService = userDetailsService;
+//    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder managerBuilder) throws Exception {
-        managerBuilder.inMemoryAuthentication()
-                .withUser("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("USER");
+        managerBuilder
+                .userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder());
     }
 
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests()
+        httpSecurity
+                .authorizeRequests()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/cinema-halls").hasRole("ROLE_ADMIN")
+                .antMatchers(HttpMethod.POST,"/movies").hasRole("ROLE_ADMIN")
+                .antMatchers(HttpMethod.POST,"/movie-sessions").hasRole("ROLE_ADMIN")
+                .antMatchers(HttpMethod.GET,"/orders").hasRole("ROLE_ADMIN")
+                .antMatchers(HttpMethod.GET,"/shopping-carts/by-user").hasRole("ROLE_ADMIN")
+                .antMatchers(HttpMethod.GET,"/users/by-email").hasRole("ROLE_ADMIN")
+                .antMatchers(HttpMethod.GET,"/users/inject").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
