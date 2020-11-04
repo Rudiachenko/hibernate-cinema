fc46df5088eefb618a8bcab01529dc02c9527952
Fedia <fedia.rudiachenko@gmail.com>
2020-11-04 14:50:33 +0200

    fixed some moments

--- src/main/java/com/dev/cinema/security/CustomUserDetailsService.java
+++ src/main/java/com/dev/cinema/security/CustomUserDetailsService.java
@@ -1,7 +1,6 @@
 package com.dev.cinema.security;
 
 import com.dev.cinema.service.UserService;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,13 +20,11 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         com.dev.cinema.model.User user = userService.findByEmail(email);
-        User.UserBuilder builder = org.springframework.security.core
-                .userdetails
-                .User.withUsername(email);
+        User.UserBuilder builder =
+                org.springframework.security.core.userdetails.User.withUsername(email);
         builder.password(user.getPassword());
         String[] roleStrings = user.getRole().stream()
                 .map(role -> role.getRoleName().name())
-                .collect(Collectors.toList())
                 .toArray(String[]::new);
         builder.roles(roleStrings);
         return builder.build();
